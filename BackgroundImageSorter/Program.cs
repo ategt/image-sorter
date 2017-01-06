16541076f6babf1f136a3dcc6b3003d059b5a04b
ATegtmeier <ategtmei@gmail.com>
2017-01-05 20:02:37 -0500

    Add project files.

---
+++ BackgroundImageSorter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackgroundImageSorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
