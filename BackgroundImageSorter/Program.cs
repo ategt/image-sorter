d85a40080282dee204bb5be9c9f59d94d28eddcf
ATegtmeier <ategtmei@gmail.com>
2017-01-10 21:38:16 -0500

    I commented out some useless things.

--- BackgroundImageSorter/Program.cs
+++ BackgroundImageSorter/Program.cs
@@ -12,8 +12,17 @@ namespace BackgroundImageSorter
         {
             System.Drawing.Image image = System.Drawing.Image.FromFile(@"C:\Users\ATeg\Desktop\test_images\20151105_090845.jpg");
             System.Drawing.Size imageSize = image.Size;
+            image.Dispose();
             //MessageBox.Show("Width: " + );
             Console.WriteLine($"Width: {imageSize.Width}, Height: {imageSize.Height}");
+
+            // This was lame. It is a guid.
+            //Console.WriteLine($"Format:  {image.RawFormat.ToString()}");
+            
+            //foreach (System.Drawing.Imaging.PropertyItem  item in image.PropertyItems)
+            //{
+            //    item.
+            //}
         }
     }
 }
